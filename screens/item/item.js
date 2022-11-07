310a379727b3e636b9ccaa75658a84ab4c6b8abd
ClaudiaSimonetti <cnsimonetti@hotmail.com>
2022-11-06 22:07:24 -0300

    cambios en el diseño

--- screens/item/item.js
+++ screens/item/item.js
@@ -1,33 +1,37 @@
 import React from "react";
-import { Text, View, Button } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux'
 import { addToCart } from "../../store/actions/cartAction";
+import GenericButton from "../../components/button/button"
+import { styles } from "./styles";
+import { colors } from "../../constants/colors/colors";
 
 function Item({navigation, route}){
-
     const dispatch = useDispatch();
-
     const productFiltered = useSelector((state)=> state.product.selected)
-
-//     function addCartItem(){
-//     dispatch(addToCart(productFiltered))
-// }
-const addCartItem=()=>{
-    dispatch(addToCart(productFiltered))
-}
-
-const {name, description, price, quantity} = productFiltered || {};
+    const addCartItem=()=>{
+        dispatch(addToCart(productFiltered))
+    }
+    const {img, name, color, size, description, price, quantity} = productFiltered || {};
     return(
         <View>
-            <Text>{name}</Text>
-            <Text>{description}</Text>
-            <Text>{price}</Text>
+            <Image
+                source={{ uri: img }}
+                style={{width: 400, height: 400}} 
+            />
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.priceDetail}>Color: {color}</Text>
+            <Text style={styles.priceDetail}>Talle: {size}</Text>
+            <Text style={styles.priceDetail}>{description}</Text>
+            <Text style={styles.priceDetail}>Precio: $ {price}</Text>
             <Text>{quantity}</Text>
-        
-        <Button
-            title="Add To cart"
-            onPress={addCartItem}
-        />
+            <View style={styles.button}>
+                <GenericButton
+                    title="Añadir al carrito"
+                    onPress={addCartItem}
+                    color={colors.black}
+                />
+            </View>
         </View>
     )
 }
---
+++ screens/item/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    title:{
+        fontFamily:'Quicksand',
+        fontSize: 25,
+        fontWeight: 'bold',
+    },
+    priceDetail:{
+        fontFamily: 'Quicksand',
+        fontSize: 16,
+    },
+    button:{
+        width: '50%',
+        marginLeft: '25%',
+    }
+})
